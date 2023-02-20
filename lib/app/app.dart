c8a90dca3c9bc3dd748d3fc8345d88abf75a7e74
Ümit Kar <umitkar08@gmail.com>
2023-02-20 14:47:38 +0300

    Created theme_manager.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm/presentation/theme_manager.dart';
 
 class MyApp extends StatefulWidget {
   //const MyApp({super.key}); // default constructor
@@ -17,6 +18,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: getAppTheme(),
+    );
   }
 }
