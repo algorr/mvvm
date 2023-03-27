5ff4a30f24ce0bc6b59ab5b34b8da62456fd38f5
Ümit Kar <umitkar08@gmail.com>
2023-03-27 10:47:00 +0300

    Added pretty_dio_logger and LanguageManager

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: constant_identifier_names
 
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:mvvm/app/constants.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 const String APPLICATION_JSON = "application/json";
 const String CONTENT_TYPE = "content_type";
@@ -26,6 +28,13 @@ class DioFactory {
         connectTimeout: Duration(minutes: timeOut),
         receiveTimeout: Duration(minutes: timeOut),
         headers: headers);
+
+    if (kReleaseMode) {
+      print('release mode no logs');
+    } else {
+      dio.interceptors.add(PrettyDioLogger(
+          requestHeader: true, requestBody: true, responseHeader: true));
+    }
 
     return dio;
   }
