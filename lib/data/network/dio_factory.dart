d05524f067c066b5b62020dc0c320fd46f5787a9
Ümit Kar <umitkar08@gmail.com>
2023-03-27 11:47:07 +0300

    created base_usecase.dart

--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -2,6 +2,7 @@
 
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:mvvm/app/app_prefs.dart';
 import 'package:mvvm/app/constants.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
@@ -12,15 +13,20 @@ const String AUTHORIZATION = "authorization";
 const String DEFAULT_LANGUAGE = "language";
 
 class DioFactory {
+  final AppPreferences _appPreferences;
+  DioFactory(this._appPreferences);
+
   Future<Dio> getDio() async {
     Dio dio = Dio();
     int timeOut = 60 * 1000; //* 1 minute
+
+    String language = await _appPreferences.getAppLanguage();
 
     Map<String, String> headers = {
       CONTENT_TYPE: APPLICATION_JSON,
       ACCEPT: APPLICATION_JSON,
       AUTHORIZATION: Constants.token,
-      DEFAULT_LANGUAGE: 'en'
+      DEFAULT_LANGUAGE: language
     };
 
     dio.options = BaseOptions(
